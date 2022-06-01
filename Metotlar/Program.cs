d1bba6b0174244f68f92f61096b8f878568f97ca
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-01 08:59:05 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Add(1.0,2.0,3.0,4.0,5.0));
+          
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Add!");
+        }
+
+        static double Add(out double a, ref double b)
+        {
+            a = b;
+            return a + b;
+        }
+
+        static double Add(params double[] nums)
+        {
+            return nums.Sum();
+        }
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var machineName = Environment.MachineName;
+            Console.WriteLine(machineName + " " + machineName.GetType());
+
+        }
+    }
+}
