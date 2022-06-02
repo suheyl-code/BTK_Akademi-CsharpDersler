78af5f3a9aa52e48f65eccd5f8adf341320e919a
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-02 10:52:31 +0300

    new project finished

--- InterfacesDemo/Program.cs
+++ InterfacesDemo/Program.cs
@@ -10,11 +10,81 @@ namespace InterfacesDemo
     {
         static void Main(string[] args)
         {
+            IWorker[] worker = new IWorker[2]
+            {
+                new Worker(),
+                new Robot()
+            };
+
+            IEater[] eater = new IEater[1]
+            {
+                new Worker()
+            };
+
+            ISalaryGetter[] salaryGetter = new ISalaryGetter[1]
+            {
+                new Worker()
+            };
+
+            foreach (var item in worker)
+            {
+                item.Work();
+            }
+            foreach (var item in eater)
+            {
+                item.Eat();
+            }
+            foreach (var item in salaryGetter)
+            {
+                item.GetYourSalary();
+            }
+
+
         }
     }
 
     interface IWorker
     {
+        void Work();
+    }
 
+    interface IEater
+    {
+        void Eat();
+    }
+
+    interface ISalaryGetter
+    {
+        void GetYourSalary();
+    }
+
+    class Worker : IWorker, IEater, ISalaryGetter
+    {
+        public string Name { get; set; } = "<Worker>";
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} able to Eat");
+        }
+
+        public void GetYourSalary()
+        {
+            Console.WriteLine($"{Name} able to get Salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} Working");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public string Name { get; set; } = "<Robot>";
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} Working");
+        }
     }
 }
