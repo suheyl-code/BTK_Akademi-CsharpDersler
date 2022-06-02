4e3b2ec2212a737585e42a57741ecb4cd406b431
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-02 12:25:59 +0300

    virtual - override

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,11 +6,43 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
+    // One thing you cannot do with Interfaces is this. Check the example here.
+    // virtual - override
+    
     internal class Program
     {
         static void Main(string[] args)
         {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+        }
+    }
 
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added to Database.");
+        }
+
+        public virtual void Update()
+        {
+            Console.WriteLine("Database Updated.");
+        }
+    }
+
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            base.Add();
+            Console.WriteLine("SQL Server <Add> Functionality applied.");
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            Console.WriteLine("SQL Server <Update> Functionality applied.");
         }
     }
 }
