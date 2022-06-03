0d969e62d636a84165015e2e30f0d15d68c457e7
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-03 13:19:46 +0300

    new project: Exceptions

---
+++ TryCatchError/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchError
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SimpleTryCatch();
+
+            // Making own Exception class
+            try
+            {
+                RecordFinding();
+            }
+            catch (RecordNotFoundException exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+
+
+            // Same as above but with Action Deligate, more advanced technique
+            RecordNotFoundException.HandleException(() =>
+            {
+                RecordFinding();
+            });
+        }
+        
+
+        private static void RecordFinding()
+        {
+            // Using Own Exception Class
+            string[] myNames = new string[3] { "Alex", "Amy", "Josef" };
+            if (!myNames.Contains("Bob"))
+            {
+                throw new RecordNotFoundException("Record Not Found!");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+
+        private static void SimpleTryCatch()
+        {
+            try
+            {
+                string[] myNames = new string[3] { "Alex", "Amy", "Josef" };
+                myNames[3] = "Bob";
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+    }
+}
---
+++ TryCatchError/RecordNotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Making own exception class for handling specific problems.
+namespace TryCatchError
+{
+    public class RecordNotFoundException : Exception
+    {
+        public RecordNotFoundException(string message) : base (message)
+        {
+
+        }
+
+        public static void HandleException(Action value)
+        {
+            try
+            {
+                value.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
